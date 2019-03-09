7ce588c56edbf81c6fed9ff02724c048d4764e28
birdmi <zzdog34@gmail.com>
2019-03-10 01:47:22 +0900

    i

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -11,6 +11,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'y4$xf4e!!l+6r2zfpp=3##(s@hbvj2_v2$m+3%_hc$jooct$7k'
+import os
+
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'y4$xf4e!!l+6r2zfpp=3##(s@hbvj2_v2$m+3%_hc$jooct$7k')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
@@ -33,8 +35,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -127,17 +129,3 @@ MEDIA_URL = '/media/'
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
-
-# The absolute path to the directory where collectstatic will collect static files for deployment.
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# The URL to use when referring to static files (where they will be served from)
-STATIC_URL = '/static/'
-
-# Simplified static file serving.
-# https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
