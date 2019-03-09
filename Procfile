b39813adb9507f88fe22d7cdb6ad05285e6907d6
birdmi <zzdog34@gmail.com>
2019-03-10 00:53:17 +0900

    a

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn blogproject.wsgi —log-file -
--- blogproject/settings.py
+++ blogproject/settings.py
@@ -10,10 +10,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'y4$xf4e!!l+6r2zfpp=3##(s@hbvj2_v2$m+3%_hc$jooct$7k'
+# SECRET_KEY = 'y4$xf4e!!l+6r2zfpp=3##(s@hbvj2_v2$m+3%_hc$jooct$7k'
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'y4$xf4e!!l+6r2zfpp=3##(s@hbvj2_v2$m+3%_hc$jooct$7k')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
 
@@ -34,6 +37,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,3 +125,19 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.0/howto/static-files/
+
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# The URL to use when referring to static files (where they will be served from)
+STATIC_URL = '/static/‘
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
