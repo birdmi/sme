7a8409ea33fd01bc10d4cf695032718fb1d62544
birdmi <zzdog34@gmail.com>
2019-06-04 04:42:08 +0900

    blog project

--- accounts/views.py
+++ accounts/views.py
@@ -5,7 +5,8 @@ from django.contrib import auth
 def signup(request):
     if request.method == 'POST':
         if request.POST['password1'] == request.POST['password2']:
-            user = User.objects.create_user(username=request.POST['username'], password=request.POST['password'])
+            user = User.objects.create_user(
+                request.POST['username'], password=request.POST['password1'])
             auth.login(request, user)
             return redirect('home')
     return render(request, 'signup.html')
---
+++ blogproject/lamen.sql
@@ -0,0 +1,5 @@
+select * from lamen;
+
+insert into lamen values('신라면','1790', 0, 8, 0, 505);
+insert into lamen values('열라면','2652', NULL, null, null, 617);
+insert into lamen values('참깨라면', '2246', 13, 10, 0, 612);
